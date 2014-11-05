1080ab697e0ba84fdca78bf2c00871b9c7ef72e6
bertt <btemme@hotmail.com>
2014-11-05 12:36:13 +0100

    first

---
+++ UnitySources/ClassLibrary1/SampleBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace ClassLibrary1
+{
+    public class SampleBehaviour : MonoBehaviour
+    {
+        private void Start()
+        {
+            // just set the background color to test
+            Camera.main.backgroundColor = Color.black;
+        }
+    }
+}
